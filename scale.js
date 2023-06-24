dffcdd6555fb6f76ae7e4b54bf54b49629b5d1e8
Barbariskaa <129290831+Barbariskaa@users.noreply.github.com>
2023-06-24 12:47:15 +0300

    Update scale.js

--- scale.js
+++ scale.js
@@ -1,75 +1,48 @@
-const https = require('https');
 const http = require('http');
+const axios = require('axios');
 
-//Scale deployment url: https://dashboard.scale.com/spellbook/api/v2/deploy/9lytrlq
+//Scale deployment url: https://dashboard.scale.com/spellbook/api/v2/deploy/9jm7gs9
 const SCALE_URL=""
 // API Key
 const SCALE_API=""
 
+const JSON_stringify_fixed = (obj) => JSON.stringify(obj).replace(
+  /[\u007f-\uffff]/g,
+  (c) => '\\u' + ('0000' + c.charCodeAt(0).toString(16)).slice(-4),
+);
+//Код Массетера с клодоскрипта ^ 
+
 async function fetchData(messages) {
-  return new Promise((resolve, reject) => {
-    const data = JSON.stringify({
-      input: {
-        input: messages,
-      },
-    });
-    const url = new URL(SCALE_URL);
-    const options = {
-      hostname: url.hostname,
-      path: url.pathname,
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json; charset=utf-8',
-        'Content-Length': Buffer.byteLength(data),
-        Authorization: `Basic ${SCALE_API}`,
-      },
-    };
-    const req = https.request(options, (res) => {
-      let responseData = '';
-      res.on('data', (chunk) => {
-        responseData += chunk;
-      });
-      res.on('end', () => {
-        // Check if responseData contains Cloudflare Error 1020
-        // Check if responseData is a valid JSON before attempting to parse it
-        try {
-          const parsedData = JSON.parse(responseData);
-          resolve(parsedData.output);
-        } catch (error) {
-          // If parsing fails, reject with an error
-          console.log(responseData)
-          if(responseData=="error code: 1020") reject(responseData + "\n(Cloudflare: Access denied. Maybe you need to turn on VPN or something)");
-          reject(responseData)
-        }
-      });
-    });
-    req.on('error', (error) => {
-      console.log(error)
-      reject(error);
-    });
-    req.write(data);
-    req.end();
+  const data = {
+    input: {
+      input: JSON_stringify_fixed(messages)
+    }
+  }
+  const headers = { 
+    'Content-Type': 'application/json; charset=utf-8',
+    'Content-Length': Buffer.byteLength(JSON_stringify_fixed(data)),
+    Authorization: `Basic ${SCALE_API}`
+  }
+  const result = await axios.post(SCALE_URL, data, {
+      headers
   });
+  return result.data.output
 }
 
+const readBody = (res) => new Promise((resolve, reject) => {
+  let buffer = '';
 
-const readBody = (res, json, onData) => new Promise((resolve, reject) => {
-    let buffer = '';
+  res.on('data', chunk => {
+      buffer += chunk;
+  });
 
-    res.on('data', chunk => {
-        onData?.(chunk.toString());
-        buffer += chunk;
-    });
-
-    res.on('end', () => {
-        try {
-            if (json) buffer = JSON.parse(buffer);
-            resolve(buffer);
-        } catch (e) {
-            console.error(buffer);
-            reject(e);
-        }
-    });
+  res.on('end', () => {
+      try {
+          resolve(JSON.parse(buffer));
+      } catch (e) {
+          reject(e);
+      }
+  });
 })
 
 async function main() {
@@ -80,7 +53,6 @@ async function main() {
                 messages,
             } = body;
             res.setHeader('Content-Type', 'application/json');
-            console.log("got body\n",messages)
             const id = `chatcmpl-${(Math.random().toString(36).slice(2))}`;
             const created = Math.floor(Date.now() / 1000);
 
@@ -89,11 +61,10 @@ async function main() {
             try {
                 result = await fetchData(JSON.stringify(messages));
                 console.log("Fetched. Scale response:\n\n", result)
-                console.log("Preparing to send completion")
+                console.log("\n\n",result.replace(/\\n/g, '\n').replace(/\\"/g, '"').trim())
              } catch (error) {
                 result = error
              }             
-             console.log(result)
             res.write(JSON.stringify({
                 id, created,
                 object: 'chat.completion',
@@ -101,19 +72,19 @@ async function main() {
                 choices: [{
                     message: {
                         role: 'assistant',
-                        content: result,
+                        content: result.replace?.(/\\n/g, '\n').replace(/\\"/g, '"')
                     },
                     finish_reason: 'stop',
                     index: 0,
                 }]
-            }, (key, value) => (typeof value === "string") ? value.replace(/\\\\/g, "\\") : value)); // unescaping slashes
+            }))
             res.end();
         } else {
             res.setHeader('Content-Type', 'application/json');
             res.write(JSON.stringify({
                 object: 'list',
                 data: [
-                    { id: 'GPT-4 filtered', object: 'model', created: Date.now(), owned_by: 'OpenAI', permission: [], root: 'GPT-4 filtered', parent: null },
+                    { id: 'GPT-4', object: 'model', created: Date.now(), owned_by: 'OpenAI', permission: [], root: 'GPT-4 filtered', parent: null },
                 ]
             }));
         }
